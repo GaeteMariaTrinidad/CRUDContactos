03c18b1ed6c9e9ebe12be75e0301182d42e068eb
María Trinidad <mar.gaetem@duocuc.cl>
2025-03-18 23:45:50 -0300

    up

---
+++ CRUDContactos/Datos/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace CRUDContactos.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+
+        public Conexion() {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            cadenaSQL = builder.GetSection("ConnectionStrings:CadenaSQL").Value;
+        }
+        public string getCadenaSQL() {
+            return cadenaSQL;
+        }
+    }
+}
---
+++ CRUDContactos/Datos/ContactoDatos.cs
@@ -0,0 +1,38 @@
+﻿using CRUDContactos.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUDContactos.Datos
+{
+    public class ContactoDatos
+    {
+        public List<ContactoModel> Listar()
+        {
+            var oLista = new List<ContactoModel>();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            IdContacto = Convert.ToInt32(dr["IdContacto"]),
+                            Nombre = dr["Nombre"].ToString(),
+                            Telefono = dr["Telefono"].ToString(),
+                            Correo = dr["Correo"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+    }
+}
